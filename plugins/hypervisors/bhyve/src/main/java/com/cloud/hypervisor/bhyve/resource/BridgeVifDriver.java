f0ed845de6ad0f3b2a73d6cd051f6d3dda8a03fc
Luís A. Bastião Silva <luis.kop@gmail.com>
2018-09-02 16:11:51 +0100

    Added support to get bridges in FreeBSD.

--- plugins/hypervisors/bhyve/src/main/java/com/cloud/hypervisor/bhyve/resource/BridgeVifDriver.java
+++ plugins/hypervisors/bhyve/src/main/java/com/cloud/hypervisor/bhyve/resource/BridgeVifDriver.java
@@ -87,7 +87,71 @@ public class BridgeVifDriver extends VifDriverBase {
         }
     }
 
+
+    private void getPifsFreeBSD(){
+
+    }
+
+    private List<String> getBridgesFreeBSD(){
+        // ifconfig -a | sed -E 's/[[:space:]:].*//;/^$/d' | grep bridge
+        final List<String> bridges = new ArrayList<String>();
+        String listOfInterfaces = Script.runSimpleShScript("ifconfig -a | sed -E 's/[[:space:]:].*//;/^$/d' | grep bridge");
+        String [] bridgesTmp = listOfInterfaces.split("\n");
+        for (int i = 0 ; i<bridgesTmp.length; i++){
+            bridges.add(bridgesTmp[i]);
+        }
+        return bridges;
+
+    }
+
     public void getPifs() {
+        final List<String> bridges = getBridgesFreeBSD();
+
+        String guestBridgeName = _libvirtComputingResource.getGuestBridgeName();
+        String publicBridgeName = _libvirtComputingResource.getPublicBridgeName();
+
+        for (final String bridge : bridges) {
+            s_logger.debug("looking for pif for bridge " + bridge);
+            final String pif = getPif(bridge);
+            if (_libvirtComputingResource.isPublicBridge(bridge)) {
+                _pifs.put("public", pif);
+            }
+            if (guestBridgeName != null && bridge.equals(guestBridgeName)) {
+                _pifs.put("private", pif);
+            }
+            _pifs.put(bridge, pif);
+        }
+
+        // guest(private) creates bridges on a pif, if private bridge not found try pif direct
+        // This addresses the unnecessary requirement of someone to create an unused bridge just for traffic label
+        if (_pifs.get("private") == null) {
+            s_logger.debug("guest(private) traffic label '" + guestBridgeName + "' not found as bridge, looking for physical interface");
+            final File dev = new File("/sys/class/net/" + guestBridgeName);
+            if (dev.exists()) {
+                s_logger.debug("guest(private) traffic label '" + guestBridgeName + "' found as a physical device");
+                _pifs.put("private", guestBridgeName);
+            }
+        }
+
+        // public creates bridges on a pif, if private bridge not found try pif direct
+        // This addresses the unnecessary requirement of someone to create an unused bridge just for traffic label
+        if (_pifs.get("public") == null) {
+            s_logger.debug("public traffic label '" + publicBridgeName+ "' not found as bridge, looking for physical interface");
+            final File dev = new File("/sys/class/net/" + publicBridgeName);
+            if (dev.exists()) {
+                s_logger.debug("public traffic label '" + publicBridgeName + "' found as a physical device");
+                _pifs.put("public", publicBridgeName);
+            }
+        }
+
+        s_logger.debug("done looking for pifs, no more bridges");
+
+
+
+    }
+
+    public void getPifsLinux() {
+
         final File dir = new File("/sys/devices/virtual/net");
         final File[] netdevs = dir.listFiles();
         final List<String> bridges = new ArrayList<String>();
